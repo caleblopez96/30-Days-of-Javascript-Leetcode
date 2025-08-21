3ec7770effcc78f45436397808e1a07babcbf66f
caleblopez96 <caleblopez96@gmail.com>
2025-08-20 18:46:13 -0700

    Moved problem descriptoion to md file

--- Hello World Function/helloWorldFunction.js
+++ Hello World Function/helloWorldFunction.js
@@ -1,11 +1,7 @@
-/*
-Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-*/
-
-// solution:
 /**
  * @return {Function}
  */
+
 function createHelloWorld() {
   return function (...args) {
     return "Hello World";
--- Hello World Function/helloWorldFunction.ts
+++ Hello World Function/helloWorldFunction.ts
@@ -1,9 +1,3 @@
-/*
-Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-Any arguments could be passed to the function but it should still always return "Hello World".
-*/
-
 // Solution:
 
 //@ts-ignore: duplicate function name
